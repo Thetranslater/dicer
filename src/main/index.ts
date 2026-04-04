387c2d3b29cf1c27be8f0abeb9e3b26f3e63f747
广宇 马 <1900390844@qq.com>
2026-04-04 21:38:04 +0800

    v1.1.0

--- src/main/index.ts
+++ src/main/index.ts
@@ -37,13 +37,8 @@ function setupAutoUpdate() {
   const { autoUpdater } = electronUpdater;
 
   autoUpdater.autoDownload = true
-  autoUpdater.autoInstallOnAppQuit = true
+  autoUpdater.autoInstallOnAppQuit = false
 
-  autoUpdater.on('error', (e) => console.error('[updater] error', e))
-  autoUpdater.on('checking-for-update', () => console.log('[updater] checking'))
-  autoUpdater.on('update-available', (info) => console.log('[updater] available', info.version))
-  autoUpdater.on('update-not-available', () => console.log('[updater] not available'))
-  autoUpdater.on('download-progress', (p) => console.log('[updater] progress', p.percent))
   autoUpdater.on('update-downloaded', async () => {
     const { response } = await dialog.showMessageBox({
       type: 'info',
@@ -56,62 +51,6 @@ function setupAutoUpdate() {
 
   autoUpdater.checkForUpdates()
 }
-
-// 娴嬭瘯涓婁紶鍥剧墖
-// function testUploadImage(): void {
-//   const imagePath = join(__dirname, '../../test.jpg')
-//   console.log('璇诲彇鍥剧墖璺緞:', imagePath)
-
-//   const imageBuffer = readFileSync(imagePath)
-//   console.log('鍥剧墖澶у皬:', imageBuffer.length, 'bytes')
-//   const params = {
-//     'v2': '1',
-//     'func': 'upload',
-//     'fid': '-447601',
-//     'auth': '03b0912569b2c835462d396c6464a39f71a0008b8631051b8f34ab976a56',
-//     'attachment_file1': imageBuffer.toString(),
-//     'attachment_file1_dscp': 'test image',
-//     'attachment_file1_url_utf8_name': encodeURIComponent('test.jpg'),
-//   }
-//   const baseUrl = 'https://img8.nga.cn/attach.php'
-//   const url = new URL(baseUrl)
-//   for (const key in params) {
-//     if (key === 'attachment_file1') {
-//       continue // 浜岃繘鍒舵暟鎹崟鐙锟?    }
-//     url.searchParams.append(key, params[key as keyof typeof params])
-//     }
-//   }
-
-//   const request = net.request({
-//     method: 'POST',
-//     protocol: 'https:',
-//     url: 'https://bbs.nga.cn/index.php?lite=js',
-//     headers: {
-//       'Cookie': 'Hm_lvt_01c4614f24e14020e036f4c3597aa059=1774409549; HMACCOUNT=3866E0CF232BF660; __ad_cookie_mapping_tck_731=0527c107441bef0cc49db488585794dc; ngacn0comUserInfo=UIDIAIPI%09UIDIAIPI%0939%0939%09%0910%090%094%090%090%09192_20%2C130_10; ngaPassportUid=61903141; ngaPassportUrlencodedUname=UIDIAIPI; ngaPassportCid=X8uiqit3hkql9fnnmd7l461vjnvhpb65s1gomg7u; ngacn0comUserInfoCheck=c23f1e8669136a6c99d0649e751f6f7c; ngacn0comInfoCheckTime=1774411622; lastpath=/; lastvisit=1774423241; bbsmisccookies=%7B%22pv_count_for_insad%22%3A%7B0%3A-22%2C1%3A1774458088%7D%2C%22insad_views%22%3A%7B0%3A1%2C1%3A1774458088%7D%2C%22uisetting%22%3A%7B0%3A%22d%22%2C1%3A1774423541%7D%7D; Hm_lpvt_01c4614f24e14020e036f4c3597aa059=1774423242',
-//       'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36 Edg/145.0.0.0',
-//       'Referer': 'https://nga.cn/',
-//       'Accept': '*/*',
-//       'Accept-Language': 'zh-CN,zh;q=0.9',
-//       'Connection': 'keep-alive',
-//       'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundary'
-//     }
-//   })
-
-//   request.on('response', (response) => {
-//     console.log(`STATUS: ${response.statusCode}`)
-//     console.log(`HEADERS: ${JSON.stringify(response.headers)}`)
-//     let data = ''
-//     response.on('data', (chunk) => {
-//       data += chunk.toString()
-//     })
-//     response.on('end', () => {
-//       console.log('涓婁紶鍝嶅簲:', data)
-//     })
-//   })
-
-//   //request.write(fullBody)
-//   request.end()
-// }
 
 protocol.registerSchemesAsPrivileged([
   {
@@ -130,7 +69,7 @@ protocol.registerSchemesAsPrivileged([
 app.whenReady().then(() => {
 
   setupAutoUpdate()
-  // 娉ㄥ唽鏈湴鏂囦欢鍗忚
+
   registerLocalProtocol()
 
   registerCoreIpcHandlers()
